4b8bfa3b77186a3c7652808d2e0f7c12a658929e
tracey jaquith <tracey@archive.org>
2019-02-23 08:49:28 +0000

    future upate recipe

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+cp /petabox/www/common/Arg.inc .
+cp /petabox/www/common/Video.inc .
+
+cp /petabox/deriver/derivations.xml  .
+cp /petabox/deriver/inc/Thumbnails.php   Thumbnails.inc
+cp /petabox/deriver/inc/MPEG4.php  MPEG4.inc
+
+cp /petabox/bin/ffmpeg-README.txt  .
+
+for i in ffmpeg-aac.patch  ffmpeg-PAT.patch  ffmpeg-theora.patch  ffmpeg-thumbnails.patch; do
+  rm -f $i
+  wget https://archive.org/~tracey/downloads/patches/$i
+done
